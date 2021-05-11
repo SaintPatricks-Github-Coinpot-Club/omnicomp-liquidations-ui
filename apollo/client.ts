e93a034341391b2a4649fdc6af1176feb60a197d
omni-corp-2021 <omniconsumerprotocols@gmail.com>
2021-05-11 11:17:33 +0530

    Subgraph query url changed

--- apollo/client.ts
+++ apollo/client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
 const omnicompLink = new HttpLink({
-  uri: "https://gq-omnicomp.ocp.finance",
+  uri: "https://api.thegraph.com/subgraphs/name/omni-corp-protocols/omnicomp",
 });
 
 export const client = new ApolloClient({
