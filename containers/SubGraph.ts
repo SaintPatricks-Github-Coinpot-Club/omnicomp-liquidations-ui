afa5f7a7386d04044a009e12a9f9ec1b1b80f2fe
clarence-boddicker <clarence@ocp.finance>
2021-05-18 14:15:18 +0530

    Removed unnecessary subgraph queries and filtering

--- containers/SubGraph.ts
+++ containers/SubGraph.ts
@@ -2,21 +2,9 @@ import { createContainer } from "unstated-next";
 import { useState, useEffect } from "react";
 import { useQuery } from "@apollo/client";
 
-import { ALL_ACCOUNTS, ALL_BORROWERS } from "../apollo/omnicomp/queries";
-
-const MIN_HEALTH = 0.001;
-const MAX_HEALTH = 0.999;
+import { ALL_ACCOUNTS } from "../apollo/omnicomp/queries";
 
 const useSubGraph = () => {
-  const {
-    loading: accountsLoading,
-    error: accountsError,
-    data: accountsData,
-  } = useQuery(ALL_BORROWERS, {
-    context: { clientName: "OMNICOMP" },
-    pollInterval: 10000,
-  });
-
   const {
     loading: allAccountsLoading,
     error: allAccountsError,
@@ -27,43 +15,19 @@ const useSubGraph = () => {
   });
 
   const [allAccounts, setAllAccounts] = useState<[] | null>(null);
-  const [allBorrowers, setAllBorrowers] = useState<[] | null>(null);
-  const [filteredUnhealthyAccounts, setFilteredUnhealthyAccounts] = useState<
-    [] | null
-  >(null);
 
   const queryAccounts = () => {
     if (allAccountsData) {
       setAllAccounts(allAccountsData.accounts);
     }
-
-    if (accountsData) {
-      setAllBorrowers(accountsData.accounts);
-    }
-  };
-
-  const filterAccounts = () => {
-    if (allBorrowers) {
-      const filtered: any = allBorrowers.filter(
-        (acc: any) => acc.health > MIN_HEALTH && acc.health < MAX_HEALTH
-      );
-      const sorted = filtered.sort((a: any, b: any) => a.health - b.health);
-      setFilteredUnhealthyAccounts(sorted);
-    }
   };
 
   useEffect(() => {
     queryAccounts();
-  }, [allAccountsData, accountsData]);
-
-  useEffect(() => {
-    filterAccounts();
-  }, [allBorrowers]);
+  }, [allAccountsData]);
 
   return {
     allAccounts,
-    allBorrowers,
-    filteredUnhealthyAccounts,
   };
 };
 
