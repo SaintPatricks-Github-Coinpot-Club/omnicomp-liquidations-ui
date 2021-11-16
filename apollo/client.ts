294b708d1db4b82e71604fef756d6e76daedc542
Geek4tek <emil@ocp.finance>
2021-11-16 11:04:40 +0530

    OCP-USDO REKT

--- apollo/client.ts
+++ apollo/client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
 const omnicompLink = new HttpLink({
-  uri: "https://api.thegraph.com/subgraphs/name/omni-corp-protocols/omnivault-pufflina",
+  uri: "https://api.thegraph.com/subgraphs/name/omni-corp-protocols/omnivault-ocp",
 });
 
 export const client = new ApolloClient({
